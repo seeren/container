707b44f649e68bd2b910aea42d8c00134bac9bca
Cyril <consultant@seeren.fr>
2017-06-12 18:15:54 +0200

    Add coverage

--- test/Resolver/Constructor/DocumentationResolverTest.php
+++ test/Resolver/Constructor/DocumentationResolverTest.php
@@ -134,6 +134,18 @@ class DocumentationResolverTest extends AbstractResolverTest
        parent::assertUseCache();
    }
 
+   /**
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::resolve
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::getReflection
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::getArg
+    */
+   public function testResolveOneDependencie()
+   {
+       $this->assertTrue($this->getResolver()->resolve(Qux::class)
+              instanceof Qux);
+   }
 
    /**
     * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::__construct
@@ -141,12 +153,52 @@ class DocumentationResolverTest extends AbstractResolverTest
     * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::resolve
     * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::getReflection
     * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::getArg
-    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::getClassName
     */
-   public function testResolveOneDependencie()
+   public function testResolveOneDependencieOptional()
    {
-       $this->assertTrue($this->getResolver()->resolve(Qux::class)
-                         instanceof Qux);
+       $this->assertTrue($this->getResolver()->resolve(Quux::class)
+           instanceof Quux);
+   }
+
+   /**
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::resolve
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::getReflection
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::getArg
+    * @covers \Seeren\Container\Exception\NotFoundException::__construct
+    * @expectedException \Psr\Container\NotFoundExceptionInterface
+    */
+   public function testResolveOneDependencieNotFoundException()
+   {
+       $this->getResolver()->resolve(Corge::class);
+   }
+
+   /**
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::resolve
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::getReflection
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::getArg
+    * @covers \Seeren\Container\Exception\ContainerException::__construct
+    * @expectedException \Psr\Container\ContainerExceptionInterface
+    */
+   public function testResolveOneDependencieContainerException()
+   {
+       $this->getResolver()->resolve(Grault::class);
+   }
+
+   /**
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::__construct
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::resolve
+    * @covers \Seeren\Container\Resolver\Constructor\AbstractResolver::getReflection
+    * @covers \Seeren\Container\Resolver\Constructor\DocumentationResolver::getArg
+    */
+   public function testResolveManyDependencie()
+   {
+       $this->assertTrue($this->getResolver()->resolve(Garply::class)
+           instanceof Garply);
    }
 
 }
@@ -155,9 +207,58 @@ class Qux
 {
     
     /**
-     * @param \Seeren\Container\Test\Resolver\Constructor\Bar $bar bar test
+     * @param \Seeren\Container\Test\Resolver\Constructor\Bar $bar
      */
     public function __construct(Bar $bar)
+    {
+    }
+
+}
+
+class Quux
+{
+    
+    /**
+     * Not documented
+     */
+    public function __construct(Bar $bar = null)
+    {
+    }
+
+}
+
+class Corge
+{
+
+    /**
+     * @param \BarNotFound $bar
+     */
+    public function __construct(Bar $bar)
+    {
+    }
+
+}
+
+class Grault
+{
+
+    /**
+     * @param \Seeren\Container\Test\Resolver\Constructor\Foo $foo
+     */
+    public function __construct(Foo $foo)
+    {
+    }
+
+}
+
+class Garply
+{
+
+    /**
+     * @param \Seeren\Container\Test\Resolver\Constructor\Qux $qux
+     * @param \Seeren\Container\Test\Resolver\Constructor\Bar $bar
+     */
+    public function __construct(Qux $qux, Bar $bar)
     {
     }
 
